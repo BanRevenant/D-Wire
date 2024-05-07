d85c9190591760199cebe410722dacb6a7f29811
Revenant <76490474+BanRevenant@users.noreply.github.com>
2024-05-07 00:33:02 -0600

    Update install.sh
    The branch is called master, not main!

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Download the deploy_bot.sh script
-curl -sSL https://raw.githubusercontent.com/BanRevenant/D-Wire/main/deploy_bot.sh > deploy_bot.sh
+curl -sSL https://raw.githubusercontent.com/BanRevenant/D-Wire/master/deploy_bot.sh > deploy_bot.sh
 
 # Make the script executable
 chmod +x deploy_bot.sh
