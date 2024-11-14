6d58cdbcb0ea8222da495bb9b600939d3f8f4953
Revenant <76490474+BanRevenant@users.noreply.github.com>
2024-11-14 09:42:37 -0700

    Update logging.lua
    added command logging for registration system

--- softmod/logging.lua
+++ softmod/logging.lua
@@ -16,7 +16,7 @@ end
 script.on_event(defines.events.on_player_left_game, function(event)
     local player = game.players[event.player_index]
     if player then
-        print(get_formatted_time() .. " [LEAVE] " .. player.name .. " left the game")
+        game.print(get_formatted_time() .. " [LEAVE] " .. player.name .. " left the game")
     end
 end)
 
@@ -24,7 +24,7 @@ end)
 script.on_event(defines.events.on_research_finished, function(event)
     if event and event.research then
         local research = event.research
-        print(get_formatted_time() .. " [MSG] Research " .. research.name .. " completed.")
+        game.print(get_formatted_time() .. " [MSG] Research " .. research.name .. " completed.")
     end
 end)
 
@@ -35,7 +35,7 @@ script.on_event(defines.events.on_pre_player_mined_item, function(event)
         local obj = event.entity
         if obj and obj.valid and player and player.valid then
             local position = obj.position
-            print(get_formatted_time() .. " [ACT] " .. player.name .. " mined " .. obj.name .. 
+            game.print(get_formatted_time() .. " [ACT] " .. player.name .. " mined " .. obj.name .. 
                       " [gps=" .. position.x .. "," .. position.y .. "]")
         end
     end
@@ -49,7 +49,7 @@ script.on_event(defines.events.on_built_entity, function(event)
         if obj.name ~= "entity-ghost" and 
            obj.name ~= "tile-ghost" and 
            obj.name ~= "tile" then
-            print(get_formatted_time() .. " [ACT] " .. player.name .. " placed " .. obj.name)
+            game.print(get_formatted_time() .. " [ACT] " .. player.name .. " placed " .. obj.name)
         end
     end
 end)
@@ -58,7 +58,7 @@ end)
 script.on_event(defines.events.on_player_joined_game, function(event)
     local player = game.players[event.player_index]
     if player then
-        print(get_formatted_time() .. " [JOIN] " .. player.name .. " joined the game")
+        game.print(get_formatted_time() .. " [JOIN] " .. player.name .. " joined the game")
     end
 end)
 
@@ -69,10 +69,10 @@ script.on_event(defines.events.on_player_died, function(event)
     
     if player then
         if cause then
-            print(get_formatted_time() .. " [MSG] " .. player.name .. " was killed by " .. cause.name .. " at [gps=" ..
+            game.print(get_formatted_time() .. " [MSG] " .. player.name .. " was killed by " .. cause.name .. " at [gps=" ..
                       player.position.x .. "," .. player.position.y .. "]")
         else
-            print(get_formatted_time() .. " [MSG] " .. player.name .. " died at [gps=" ..
+            game.print(get_formatted_time() .. " [MSG] " .. player.name .. " died at [gps=" ..
                       player.position.x .. "," .. player.position.y .. "]")
         end
     end
@@ -83,7 +83,7 @@ script.on_event(defines.events.on_console_chat, function(event)
     if event.player_index then
         local player = game.players[event.player_index]
         if player and event.message then
-            print(get_formatted_time() .. " [CHAT] " .. player.name .. ": " .. event.message)
+            game.print(get_formatted_time() .. " [CHAT] " .. player.name .. ": " .. event.message)
         end
     end
 end)
@@ -95,6 +95,31 @@ script.on_event(defines.events.on_tick, function(event)
         for _, player in pairs(game.connected_players) do
             table.insert(players, player.name)
         end
-        print("[ONLINE2] " .. table.concat(players, ", "))
+        game.print("[ONLINE2] " .. table.concat(players, ", "))
     end
 end)
+
+-- Command logging
+function on_console_command(event)
+    if event and event.command and event.parameters then
+        local command = ""
+        local args = ""
+
+        if event.command then
+            command = event.command
+        end
+
+        if event.parameters then
+            args = event.parameters
+        end
+
+        if event.player_index then
+            local player = game.players[event.player_index]
+            print(string.format("[CMD] NAME: %s, COMMAND: %s, ARGS: %s", player.name, command, args))
+        elseif command ~= "time" and command ~= "online" and command ~= "server-save" and command ~= "p" then -- Ignore spammy console commands
+            print(string.format("[CMD] NAME: CONSOLE, COMMAND: %s, ARGS: %s", command, args))
+        end
+    end
+end
+
+script.on_event(defines.events.on_console_command, on_console_command)
